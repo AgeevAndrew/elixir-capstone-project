f7014e6856f1833776a2e72c06c6d122b2a23ac1
Andrew Ageev <ageev86@gmail.com>
2021-11-18 23:12:27 +0300

    First commit

---
+++ lib/capstone_project.ex
@@ -0,0 +1,18 @@
+defmodule CapstoneProject do
+  @moduledoc """
+  Documentation for `CapstoneProject`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CapstoneProject.hello()
+      'Hello world'
+
+  """
+  def hello do
+    'Hello world'
+  end
+end
---
+++ test/capstone_project_test.exs
@@ -0,0 +1,8 @@
+defmodule CapstoneProjectTest do
+  use ExUnit.Case
+  doctest CapstoneProject
+
+  test "greets the world" do
+    assert CapstoneProject.hello() == 'Hello world'
+  end
+end
